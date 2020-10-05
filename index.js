cb0296899a6ac632d9c4f20e6af7dfcb3b5fd791
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-10-05 23:36:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 // Write your code here
-const variable1 = 2
-const variable2 = 31
+const num1 = 2
+const num2 = 31
 
-const multiply = variable1 * variable2
+const multiply = num1 * num2
 
 const random = Math.floor(Math.random() * 1000) + 1
 
